453ac1e211cdb5fb5de45fe34a3c39f1723e7fd1
Magomed-DMB <06mohmad1416@gmail.com>
2026-06-25 17:05:41 +0300

    first commit

--- generate-hash.js
+++
@@ -1,7 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-const password = 'admin123'; // Ваш пароль
-const hash = await bcrypt.hash(password, 10);
-
-console.log('Хеш для пароля "' + password + '":');
-console.log(hash);
